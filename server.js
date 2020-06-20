f4719d866cb13fae5edf39686db2b1379f70ff5c
Elizabeth Hayt <enhayt@gmail.com>
2020-06-20 11:26:51 -0700

    Enable express server to serve front-end

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3002, () => {
   console.log('Express server listening on port 3002');

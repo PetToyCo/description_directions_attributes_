056d67bdead14c551dd4479f9b253b63e07f20e3
Elizabeth Hayt <enhayt@gmail.com>
2020-06-23 09:33:34 -0700

    Create axios request in componentDidMount and setState with new data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,15 +2,33 @@ import Description from './Components/Description.jsx';
 import Directions from './Components/Directions.jsx';
 import Attributes from './Components/Attributes.jsx';
 import Additional from './Components/Additional.jsx';
+import axios from 'axios';
 
 class DescriptionService extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      current: 'description'
+      current: 'description',
+      data: {}
     }
   }
+
+  componentDidMount() {
+    //call endpoint for desc obj
+    axios.get('/descriptionObject/100')
+      .then(data => {
+        console.log('success getting data in componentDidMount: ', data);
+        this.setState({
+          data: data.data
+        });
+        console.log('state: ', this.state);
+      })
+      .catch(err => {
+        console.log('error getting descObj in componentDidMount: ', err);
+      });
+  }
+
 
   render() {
     return (
@@ -21,10 +39,10 @@ class DescriptionService extends React.Component {
           <button>Attributes/Specifications</button>
           <button>Additional Details</button>
         </div>
-        {this.state.current === 'description' && <Description />}
-        {this.state.current === 'directions' && <Directions />}
-        {this.state.current === 'attributes' && <Attributes />}
-        {this.state.current === 'additional' &&<Additional />}
+        {this.state.current === 'description' && <Description description={this.state.data.description}/>}
+        {this.state.current === 'directions' && <Directions directions={this.state.data.directions}/>}
+        {this.state.current === 'attributes' && <Attributes attributes={this.state.data.attributes}/>}
+        {this.state.current === 'additional' && <Additional additional={this.state.data.additional}/>}
       </div>
     )
   }

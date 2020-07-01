9eb7ebecb7b01d679a2124dcf456f439aa0ee74c
Elizabeth Hayt <enhayt@gmail.com>
2020-07-01 16:35:24 -0700

    Remove unnecessary bodyParser

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('./database-mongodb/index.js');
 
@@ -12,8 +11,6 @@ app.use((req, res, next) => {
 })
 
 app.use(express.static(path.join(__dirname, 'client/public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 
 //get title and brand name for an item
